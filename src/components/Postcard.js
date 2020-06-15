3b5b548cc5bb81a228684cade8d611c906128673
builtforsomethingbigger <builtforsomethingbigger@gmail.com>
2020-06-15 08:37:06 -0400

    Homepage Update - Postcard styling applied

--- src/components/Postcard.js
+++ src/components/Postcard.js
@@ -6,28 +6,37 @@ function Postcard(props) {
   const { title, description, likes, url_link } = props
   console.log(props)
   return (
-    <div className="ui items">
-      <div className="item">
-        <div className="image">
-          <img src={url_link} alt='#'/>
-        </div>
-        <div className="content">
-          <a className="header">{title}</a>
-          <div className="meta">
-            <span>{description}</span>
-          </div>
-          <div className="description">
-            <p></p>
-          </div>
-          <div className="extra">
-            likes: {likes}
-          </div>
+    // <div className="ui items">
+    //   <div className="item">
+    //     <div className="image">
+    //       <img src={url_link} alt='#'/>
+    //     </div>
+    //     <div className="content">
+    //       <a className="header">{title}</a>
+    //       <div className="meta">
+    //         <span>{description}</span>
+    //       </div>
+    //       <div className="description">
+    //         <p></p>
+    //       </div>
+    //       <div className="extra">
+    //         likes: {likes}
+    //       </div>
+    //     </div>
+    //   </div>
+
+    // </div>
+
+    <div class="ui container marg_10">
+      <div class="ui left aligned table pad_20">
+        <div class="content">
+          <h1 class="header">{title}</h1>
+          <div class="description"><b>Description:</b> {description}</div>
+          <div><b>Resource Link:</b> <a class="header"> {url_link}</a></div>
+          <div className="extra"><b>likes:</b> {likes}</div>
         </div>
       </div>
-
     </div>
-
-
 
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 
 function Search() {
   return (
-    <div className="ui category search">
+    <div className="ui category search container">
       <div className="ui icon input">
         <input className="prompt" type="text" placeholder="Search by title..." />
         <i className="search icon"></i>
