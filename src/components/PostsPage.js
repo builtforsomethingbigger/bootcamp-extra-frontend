2080f5bf530915c2bea49d9728abc541663eba4c
Edgar <edgar.a.rivera08@gmail.com>
2020-06-15 22:57:46 -0400

    created a show page, with a working like button

--- src/components/PostsPage.js
+++ src/components/PostsPage.js
@@ -2,15 +2,16 @@ import React from 'react';
 import Postcard from './Postcard'
 
 
+
 class PostsPage extends React.Component {
 
-  // console.log(props.posts)
-
+  
   render(){
+    console.log(this.props)
     return (
       <div className="PostsPage">
     
-        {this.props.posts.map(post => <Postcard key={post.id} {...post} />  )}
+        {this.props.posts.map(post => <Postcard key={post.id} {...post}  />  )}
       </div>
     );
   }
