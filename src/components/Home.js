13ce32a51a14d62d4e6aa17f9fc5cba00a41867b
builtforsomethingbigger <builtforsomethingbigger@gmail.com>
2020-06-18 09:20:24 -0400

    HomePosts Previous and Next buttons functioning

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,18 +5,18 @@ import RecentPosts from './RecentPosts';
 
 export default class Home extends React.Component {
 
-
   render() {
+
     return (
       <div className="App pad_10">
         <div className="full">
           
           
-          <table className="full" border="0" cellSpacing="0" cellPadding="0">
+          <table className="full pad_bottom_100" border="0" cellSpacing="0" cellPadding="0">
             <tbody>  
               <tr>
                 <td valign='top'>
-                  <HomePosts posts={this.props.displaySearchedPosts} authors={this.props.authors} />
+                  <HomePosts history={this.props.history} posts={this.props.displaySearchedPosts} authors={this.props.authors} />
                   {/* <div className="font_blue font_20">Loading More Posts...</div> */}
                 </td>
                 <td valign='top'>
